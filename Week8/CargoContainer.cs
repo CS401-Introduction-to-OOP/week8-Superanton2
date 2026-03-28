f338ae3dfcf9b7953692610e2cc4192091c33d97
Anton <antontv2019@gmail.com>
2026-03-28 14:33:04 +0200

    added Container

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,17 @@
+namespace Week8;
+
+public class CargoContainer<T> where T: DeliveryItem
+{
+    private List<DeliveryItem> _container;
+
+    public void AddItem(T item) => _container.Add(item);
+
+    public double GetTotalCost()
+    {
+        double sum = 0;
+        foreach (DeliveryItem item in _container)
+            sum += item.CalculateCost();
+
+        return sum;
+    }
+}
